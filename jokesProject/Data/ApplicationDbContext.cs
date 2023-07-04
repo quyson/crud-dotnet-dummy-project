d2ce31a0256954b3fc9272431a17040be0dc49b0
unknown <quyson.nguyen.1999@gmail.com>
2023-07-03 19:24:09 -0700

    Initialize joke controller

---
+++ jokesProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using jokesProject.Models;
+
+namespace jokesProject.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<jokesProject.Models.Joke> Joke { get; set; } = default!;
+    }
+}
